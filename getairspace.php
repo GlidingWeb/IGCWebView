c24b847530bf392474930a01c51ab0d1f1b25cee
Richard Brisbourne <richard@rbris.me.uk>
2015-12-18 10:59:25 +0000

    Modified to use geospatial fields and functions in mysql for airspace. getairspace.php virtually rewritten- json it delivers is unchanged, so no changes to javascript.

--- getairspace.php
+++ getairspace.php
@@ -1,50 +1,42 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
-header('Access-Control-Allow-Methods: POST');
-require_once("../db_inc.php");
 $countries=[];
-echo "{\n\"polygons\": [\n";
+require_once("../db_inc.php");
 $mysqli=new mysqli($dbserver,$username,$password,$database);
-$whereclause= "northlim  > ".floatval($_POST['minNorth'])." AND  southlim < ".floatval($_POST['maxNorth'])." AND  eastlim > ".floatval($_POST['minEast'])." AND westlim < ".floatval($_POST['maxEast']);
-//$whereclause= "northlim  >  55  AND  southlim < 58 AND  eastlim >-5  AND westlim < 0";
-$sql= "SELECT polynum,country,base, point, coords FROM polyref INNER JOIN polypoints ON polyref.polynum=polypoints.polygon WHERE $whereclause ORDER BY polynum,point";
-$polygons=$mysqli->query($sql);
-$counter=0;
-if($polygons->num_rows !==0) {
- echo "{\n";
-while($polypoint=$polygons->fetch_assoc()) {
-    if($polypoint['polynum'] !== $counter) {
-      $countries[]= $polypoint['country'];
-       if($counter > 0) {
-            echo "]\n},\n{\n";
-             }
-          echo "\"base\": ".$polypoint['base'].",\n\"coords\": [";
-          $counter=$polypoint['polynum'];
-        }
-        else {
-           echo ",";
-            }
-        echo $polypoint['coords'];
-    }
-echo "]\n}\n";
+$box="POLYGON((".$_POST['maxNorth']." ".$_POST['minEast'].",".$_POST['maxNorth']." ".$_POST['maxEast'].",".$_POST['minNorth']." ".$_POST['maxEast'].",".$_POST['minNorth']." ".$_POST['minEast'].",".$_POST['maxNorth']." ".$_POST['minEast']."))";
+$sql="SET @bbox=GeomFromText('".$box."')";
+$mysqli->query($sql);
+echo  "{\n\"polygons\": [";
+$result = $mysqli->query("SELECT country,base,AsText(outline) FROM geopoly WHERE INTERSECTS(outline,@bbox)");
+$started= false;
+if($result->num_rows !==0) {
+   while($polyinfo=$result->fetch_row()) {
+   if($started) {
+   echo ",";
+   }
+   $started="true";
+    $countries[]= $polyinfo[0];
+    echo  "\n{\n\"base\": $polyinfo[1],\n\"coords\": [[ ";
+     // not very readable but much faster than regex
+     echo str_replace(" ",",",str_replace(",","],[",str_replace(")","",str_replace("LINESTRING(","",$polyinfo[2]))))."]]\n}";
+     }
 }
-$polygons->close();
+$result->close();
 echo "],\n";
 echo "\"circles\": [\n";
- $sql2= "SELECT circlenum,country,base, centre, radius FROM circles WHERE $whereclause"; 
- $circlelist=$mysqli->query($sql2);
- $started=false;
- while($circle=$circlelist->fetch_assoc()) {
-   $countries[]= $circle['country'];
-   if($started) {
-      echo ",\n";
-      }
-  $started=true;
-   echo "{\n\"base\":".$circle['base'].",\n";
-   echo "\"centre\":".$circle['centre'].",\n";
-   echo "\"radius\":".$circle['radius']."\n}";
- }
- $circlelist->close();
+$circledata = $mysqli->query("SELECT country,base,AsText(centre),radius FROM geocircle WHERE INTERSECTS(mbr,@bbox)");
+if($circledata->num_rows !==0)  {
+    $circlestarted=false;
+    while($circleinfo=$circledata->fetch_row())  {
+       if($circlestarted) {
+       echo ",\n";
+       }
+      $countries[]= $circleinfo[0];
+      $circlestarted=true;
+      $centre=str_replace(" ",",",str_replace(")","]",str_replace("POINT(","[",$circleinfo[2])));
+    echo "{\n\"base\":$circleinfo[1],\n\"centre\":$centre,\n\"radius\":$circleinfo[3]\n}";
+    }
+}
+$circledata->close();
  $countrylist=array_unique($countries);
 if(count($countrylist) > 0) {
  $inlist= "";
@@ -73,6 +65,5 @@ else  {
   $textout= "<br>Not available for this region";
 }
 $mysqli->close();
-echo "],\n\"country\": \"$textout\"\n";
-echo "\n}\n";
+echo "\n],\n\"country\": \"$textout\"\n}\n";
 ?>
