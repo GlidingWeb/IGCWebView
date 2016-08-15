8776e2bb73dbf9c6df346a4df852d7593301563e
Richard Brisbourne <richard@rbris.me.uk>
2016-08-15 13:26:16 +0100

    Include getelevation.php previously untracked in error

---
+++ getelevation.php
@@ -0,0 +1,9 @@
+<?php
+//https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyDFNj1I-E0mP20rI__UnJH4rMxuZHUfI9M
+$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/elevation/json?locations='.$_POST['lat'].','.$_POST['lon'].'&key=AIzaSyA0gwXXqbJnT90qkme9-F3Sb3z-h7RYdYI');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $json = curl_exec($ch);
+    curl_close($ch);
+   echo $json;
+?>
